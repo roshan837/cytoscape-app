f98d391ce473d1c67eb0032cfbd40abb580ac9e8
roshan_8375 <roshan.k@healsoftware.ai>
2025-01-24 15:38:07 +0530

    adds method to create nodes and edges

--- src/data.js
+++ src/data.js
@@ -1,86 +1,3 @@
-export const elements = [
-  { "data": { "id": "node-1" }, "group": "nodes" },
-  { "data": { "id": "node-2" }, "group": "nodes" },
-  { "data": { "id": "node-3" }, "group": "nodes" },
-  { "data": { "id": "node-4" }, "group": "nodes" },
-  { "data": { "id": "node-5" }, "group": "nodes" },
-  { "data": { "id": "node-6" }, "group": "nodes" },
-  { "data": { "id": "node-7" }, "group": "nodes" },
-  { "data": { "id": "node-8" }, "group": "nodes" },
-  { "data": { "id": "node-9" }, "group": "nodes" },
-  { "data": { "id": "node-10" }, "group": "nodes" },
-  { "data": { "id": "node-11" }, "group": "nodes" },
-  { "data": { "id": "node-12" }, "group": "nodes" },
-  { "data": { "id": "node-13" }, "group": "nodes" },
-  { "data": { "id": "node-14" }, "group": "nodes" },
-  { "data": { "id": "node-15" }, "group": "nodes" },
-  { "data": { "id": "node-16", "parent": "node-5" }, "group": "nodes" },
-  { "data": { "id": "node-17", "parent": "node-5" }, "group": "nodes" },
-  { "data": { "id": "node-18", "parent": "node-5" }, "group": "nodes" },
-  { "data": { "id": "node-19", "parent": "node-1" }, "group": "nodes" },
-  { "data": { "id": "node-20", "parent": "node-1" }, "group": "nodes" },
-  { "data": { "id": "node-21", "parent": "node-5" }, "group": "nodes" },
-  { "data": { "id": "node-22", "parent": "node-1" }, "group": "nodes" },
-  { "data": { "id": "node-23", "parent": "node-5" }, "group": "nodes" },
-  { "data": { "id": "node-24", "parent": "node-5" }, "group": "nodes" },
-  { "data": { "id": "node-25", "parent": "node-1" }, "group": "nodes" },
-  { "data": { "id": "node-26", "parent": "node-31" }, "group": "nodes" },
-  { "data": { "id": "node-27", "parent": "node-31" }, "group": "nodes" },
-  { "data": { "id": "node-28", "parent": "node-31" }, "group": "nodes" },
-  { "data": { "id": "node-29", "parent": "node-31" }, "group": "nodes" },
-  { "data": { "id": "node-30", "parent": "node-31" }, "group": "nodes" },
-  { "data": { "id": "node-31", "parent": "node-2" }, "group": "nodes" },
-  { "data": { "id": "node-32", "parent": "node-2" }, "group": "nodes" },
-  { "data": { "id": "node-33", "parent": "node-2" }, "group": "nodes" },
-  { "data": { "id": "node-34", "parent": "node-2" }, "group": "nodes" },
-  { "data": { "source": "node-6", "target": "node-27", "id": "nwtE_6d4afc19-88a0-4fd4-9fbf-3591cb6ba062" }, "group": "edges" },
-  { "data": { "source": "node-15", "target": "node-20", "id": "nwtE_605f28bd-77c0-4eef-8251-c5ba9668bda7" }, "group": "edges" },
-  { "data": { "source": "node-21", "target": "node-23", "id": "nwtE_5bafa3fe-246a-477c-849c-3284c3e62578" }, "group": "edges" },
-  { "data": { "source": "node-17", "target": "node-16", "id": "nwtE_6dda445b-530e-4b95-a3b1-e09cabc73993" }, "group": "edges" },
-  { "data": { "source": "node-19", "target": "node-20", "id": "nwtE_b6195365-55fd-4e16-b03d-af46585b2618" }, "group": "edges" },
-  { "data": { "source": "node-23", "target": "node-24", "id": "nwtE_6f57baf0-3722-4012-b33e-783c267645fa" }, "group": "edges" },
-  { "data": { "source": "node-3", "target": "node-16", "id": "nwtE_9fac6ca3-d907-4b5a-8496-b0edbc3815ca" }, "group": "edges" },
-  { "data": { "source": "node-16", "target": "node-18", "id": "nwtE_ac487e12-218a-45fd-b94a-f8fb51494baa" }, "group": "edges" },
-  { "data": { "source": "node-3", "target": "node-9", "id": "nwtE_24228974-e8ba-4f05-8fe8-e775d314bcff" }, "group": "edges" },
-  { "data": { "source": "node-8", "target": "node-11", "id": "nwtE_56a86996-2c25-4071-b3a3-3000057eef90" }, "group": "edges" },
-  { "data": { "source": "node-14", "target": "node-4", "id": "nwtE_a690584d-974b-4a78-8169-584dc4aa2ef8" }, "group": "edges" },
-  { "data": { "source": "node-25", "target": "node-22", "id": "nwtE_bca25d80-d197-41ca-871c-9c3806a802c3" }, "group": "edges" },
-  { "data": { "source": "node-16", "target": "node-21", "id": "nwtE_fdd46d3d-3529-4552-bcaf-e5a43364d5eb" }, "group": "edges" },
-  { "data": { "source": "node-18", "target": "node-21", "id": "nwtE_9298d0d5-8159-4b50-b880-48aa19738a86" }, "group": "edges" },
-  { "data": { "source": "node-12", "target": "node-14", "id": "nwtE_449cf49b-88e5-44f9-9300-5a8dbd79c135" }, "group": "edges" },
-  { "data": { "source": "node-14", "target": "node-15", "id": "nwtE_15b708e3-501d-432c-941d-627df912946f" }, "group": "edges" },
-  { "data": { "source": "node-13", "target": "node-1", "id": "nwtE_fd0f48e7-988f-4707-b126-b8a04dc3f64c" }, "group": "edges" },
-  { "data": { "source": "node-6", "target": "node-13", "id": "nwtE_7049cf2c-cc2b-40ed-94b8-590e2b703c45" }, "group": "edges" },
-  { "data": { "source": "node-22", "target": "node-19", "id": "nwtE_28f94f80-370a-4819-b01e-7c14286528d6" }, "group": "edges" },
-  { "data": { "source": "node-7", "target": "node-2", "id": "nwtE_4b91ec16-80c7-476e-ac78-40ec11628f8c" }, "group": "edges" },
-  { "data": { "source": "node-21", "target": "node-24", "id": "nwtE_580dc718-3a38-4131-8527-5966dc7117bd" }, "group": "edges" },
-  { "data": { "source": "node-33", "target": "node-34", "id": "nwtE_c1260ab9-e976-4b02-a0d4-28e4e7b71956" }, "group": "edges" },
-  { "data": { "source": "node-11", "target": "node-14", "id": "nwtE_1379f27c-2858-4c7c-b305-f9dbef07f992" }, "group": "edges" },
-  { "data": { "source": "node-18", "target": "node-24", "id": "nwtE_70a9c66e-a05d-4795-9830-b941aa0bdf8d" }, "group": "edges" },
-  { "data": { "source": "node-14", "target": "node-17", "id": "nwtE_3a1a451a-396c-46dd-844a-09c8c4506788" }, "group": "edges" },
-  { "data": { "source": "node-12", "target": "node-16", "id": "nwtE_03b7f374-f923-4cbd-9b1c-358e6bd0a66a" }, "group": "edges" },
-  { "data": { "source": "node-17", "target": "node-21", "id": "nwtE_bdaaa9a5-5464-44eb-a69a-177006535c60" }, "group": "edges" },
-  { "data": { "source": "node-20", "target": "node-22", "id": "nwtE_06738526-f767-4e34-8d41-bfd8b046d48e" }, "group": "edges" },
-  { "data": { "source": "node-7", "target": "node-13", "id": "nwtE_874c0108-f1b5-4331-9580-bbc904d5ed52" }, "group": "edges" },
-  { "data": { "source": "node-4", "target": "node-23", "id": "nwtE_8ecc2707-8d9f-4c5a-b79f-36028161a2de" }, "group": "edges" },
-  { "data": { "source": "node-3", "target": "node-2", "id": "nwtE_11040a46-0530-4375-a13d-2cdca0a98536" }, "group": "edges" },
-  { "data": { "source": "node-19", "target": "node-11", "id": "nwtE_b47d4380-4724-4409-a449-1d80a798f9df" }, "group": "edges" },
-  { "data": { "source": "node-9", "target": "node-12", "id": "nwtE_b7156db8-08d3-4a8d-9b43-db3de0701017" }, "group": "edges" },
-  { "data": { "source": "node-12", "target": "node-10", "id": "nwtE_1ab89f57-598a-4805-85d6-445f44bed701" }, "group": "edges" },
-  { "data": { "source": "node-10", "target": "node-8", "id": "nwtE_3a06bd73-a5ea-42fc-bebd-6cc9b227c4d4" }, "group": "edges" },
-  { "data": { "source": "node-8", "target": "node-7", "id": "nwtE_fe9dae92-bb4d-4fbc-8b00-3275e457899b" }, "group": "edges" },
-  { "data": { "source": "node-7", "target": "node-6", "id": "nwtE_498e1c01-8c7d-4711-86d4-25119fd459b3" }, "group": "edges" },
-  { "data": { "source": "node-28", "target": "node-26", "id": "nwtE_720b0a71-9ad3-4821-b828-ecace971acd1" }, "group": "edges" },
-  { "data": { "source": "node-26", "target": "node-30", "id": "nwtE_ce9e18ee-2bb9-4abb-88f8-272b3d76a8b4" }, "group": "edges" },
-  { "data": { "source": "node-27", "target": "node-28", "id": "nwtE_6a465aa8-1c8b-4455-95bc-e67b77f3f7d3" }, "group": "edges" },
-  { "data": { "source": "node-29", "target": "node-27", "id": "nwtE_c6b430bd-17fc-4b1f-82a9-16bc0d5dfa78" }, "group": "edges" },
-  { "data": { "source": "node-30", "target": "node-28", "id": "nwtE_12dd751b-6a30-4bef-8511-d36869559740" }, "group": "edges" },
-  { "data": { "source": "node-28", "target": "node-29", "id": "nwtE_7944e4ea-bb97-484f-a6ee-0d77d7bab80f" }, "group": "edges" },
-  { "data": { "source": "node-29", "target": "node-32", "id": "nwtE_d0a8fa82-36ac-4c28-837b-aa38b8f2cdb6" }, "group": "edges" },
-  { "data": { "source": "node-32", "target": "node-33", "id": "nwtE_a4051f32-f6fe-451e-b153-e4de31f4808b" }, "group": "edges" },
-  { "data": { "source": "node-32", "target": "node-9", "id": "nwtE_70a31acd-428d-47be-a981-38107a83d2e1" }, "group": "edges" }
-]
-
 export const style = [
   {
     selector: 'node',
@@ -116,7 +33,7 @@ export function getTooltip(data) {
     <h3>${data.id}</h3>
       <div>
         <span>Label:</span> ${data.id} <br/>
-        <span>Type:</span> ${data?.type ?? 'unknown'}<br/>
+        <span>Parent:</span> ${data?.parent ?? 'None'}<br/>
         <button>More</button>
       </div>
     </div>
@@ -124,3 +41,44 @@ export function getTooltip(data) {
   content.getElementsByTagName('button')[0].addEventListener('click', onButtonClick)
   return content
 }
+
+
+function generateNodes(numNodes) {
+  const nodes = [];
+  const parentNodes = [];
+  for (let i = 1; i <= numNodes; i++) nodes.push({ data: { id: `node-${i}` }, group: "nodes" })
+
+  // Randomly select 20% of the nodes as parents
+  const parentCount = Math.floor(numNodes * 0.2);
+  while (parentNodes.length < parentCount) {
+    const randomIndex = Math.floor(Math.random() * numNodes);
+    const parentNode = nodes[randomIndex].data.id;
+    if (!parentNodes.includes(parentNode)) parentNodes.push(parentNode);
+  }
+
+  nodes.forEach(node => {
+    const randomParent = parentNodes[Math.floor(Math.random() * parentNodes.length)];
+    if (randomParent === node.data.id || Math.random() > 0.8) return // increase to increase chance of having parent
+    node.data.parent = randomParent;
+  });
+
+  return nodes;
+}
+
+function generateEdges(nodes) {
+  const edges = [];
+  for (let i = 0; i < nodes.length; i++) {
+    for (let j = i + 1; j < nodes.length; j++) {
+      if (Math.random() < .01) { // increase to increase edges
+        if (nodes[i].data.id === nodes[j].data.parent || nodes[j].data.id === nodes[i].data.parent) continue // to avoid connecting parent and child
+        edges.push({ data: { source: nodes[i].data.id, target: nodes[j].data.id, id: `${i}-${j}` }, group: "edges" });
+      }
+    }
+  }
+  return edges;
+}
+const numNodes = 100 // number of nodes
+const nodes = generateNodes(numNodes)
+const edges = generateEdges(nodes)
+
+export const elements = [...nodes, ...edges]
